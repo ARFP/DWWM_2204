bd1c037abcd732b0c79204d410bd585eb724648e
Mickaël <toto@crm.fr>
2022-08-30 11:20:11 +0200

    organisation fichiers

--- 04-bases-de-donnees/02-herbergement-social/03-Hebergement-social-DDL.sql
+++ 04-bases-de-donnees/02-herbergement-social/03-Hebergement-social-DDL.sql
@@ -19,27 +19,3 @@ CREATE TABLE people
 	role_id INT NOT NULL,
 	PRIMARY KEY(person_id)
 );
-
-INSERT INTO roles
-(role_id, role_name) 
-VALUES 
-(1, 'directeur'),
-(2, 'gardien'),
-(3, 'cuisinier'),
-(4, 'technicien'),
-(5, 'éducateur'),
-(6, 'médecin'),
-(7, 'résident');
-
-
-INSERT INTO people 
-(person_id, person_firstname, person_lastname, 
- person_birthdate, person_hiredate, 
- person_active, role_id)
-VALUES 
-(1, 'Mike', 'DEV', '1980-02-23', '2017-08-27', true, 1),
-(2, 'Toto', 'Dubizou', '1963-12-17', NULL, true, 3);
-
-SELECT * FROM roles;
-
-SELECT * FROM people;
---
+++ 04-bases-de-donnees/02-herbergement-social/05-Hebergement-social-DQL.sql
@@ -0,0 +1,4 @@
+
+SELECT * FROM roles;
+
+SELECT * FROM people;
