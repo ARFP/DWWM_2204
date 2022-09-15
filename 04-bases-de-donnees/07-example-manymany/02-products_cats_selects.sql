7f9621e012a8d329b2ea477620cd3d86023f2abe
Mickaël <toto@crm.fr>
2022-09-15 12:27:19 +0200

    Update 02-products_cats_selects.sql

--- 04-bases-de-donnees/07-example-manymany/02-products_cats_selects.sql
+++ 04-bases-de-donnees/07-example-manymany/02-products_cats_selects.sql
@@ -1,1 +1,15 @@
 
+SELECT * FROM categories;
+SELECT * FROM products;
+SELECT * FROM products_categories;
+
+-- Afficher les produits avec le nom de la catégorie principale
+SELECT reference, name, category
+FROM products 
+INNER JOIN categories ON products.main_cat_id = categories.cat_id;
+
+-- Afficher les produits avec le nom de chaque catégorie associée sauf la principale
+SELECT reference, name, category 
+FROM products 
+NATURAL JOIN products_categories -- JOIN products_categories ON products.reference = products_categories.reference
+NATURAL JOIN categories;
