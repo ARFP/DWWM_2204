3ff2705f2e16e411ea8d03d18a79fb2e4ec500ae
arfpchatelot <fchatelot@yahoo.fr>
2022-12-07 12:04:26 +0100

    chargement data codes postaux

---
+++ 05-web-frontend/01-code-postaux/script.js
@@ -0,0 +1,57 @@
+/*
+// VERSIO XMLHTTPREQUEST (ancienne méthode)
+var xhr = new XMLHttpRequest();
+xhr.open('GET', './zipcodes.json', true);
+xhr.responseType = 'json';
+xhr.onload = function() {
+    console.log(xhr.response);
+}
+xhr.send();
+
+// VERSION avec FETCH et THEN
+fetch("zipcodes.json")
+.then(response => { return response.json() })
+.then(json => { console.log(json) } )
+.catch(erreur => { console.error(erreur) })
+
+*/
+
+// VERSION AVEC FETCH et AWAIT
+try {
+ 
+    let response = await fetch("zipcodes.json");
+    var json = await response.json();
+    console.log(json);
+    console.log("toto");
+
+
+} catch(erreur) {
+    console.error(erreur);
+}
+
+
+// EVENEMENT CLICK SUR VALIDER
+const btn = document.getElementById("btnValid");
+const input = document.getElementById("toto");
+
+btn.addEventListener("click", function()
+{
+    console.log(input.value);
+    let valeurAChercher = input.value;
+
+   for(let i = 0; i < json.length; i++) {
+    let ville = json[i];
+
+    /**
+     * Recherher toutes les villes dont le nom contient "valeurARechercher"
+     */
+    if(valeurAChercher === ville.codePostal || valeurAChercher === ville.nomCommune) {
+        console.log(ville.nomCommune + " " + ville.codePostal);
+    }
+   }
+
+   for(let ville of json) {
+
+   }
+
+})
